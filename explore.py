ceddcdeb0455bbd4253055324f5effa8dd2d9674
Sarah <klute.s@northeastern.edu>
2024-09-04 10:27:50 -0400

    .gitingnore

---
+++ explore.py
@@ -0,0 +1,17 @@
+# Download the helper library from https://www.twilio.com/docs/python/install
+import os
+from twilio.rest import Client
+
+# Find your Account SID and Auth Token at twilio.com/console
+# and set the environment variables. See http://twil.io/secure
+account_sid = os.environ["TWILIO_ACCOUNT_SID"]
+auth_token = os.environ["TWILIO_AUTH_TOKEN"]
+client = Client(account_sid, auth_token)
+
+message = client.messages.create(
+    from_="+18668061186", 
+    body="Hi from Link Health", 
+    to="+12064229016"
+)
+
+print(f'SID: {message.body} Status: {message.status}')
